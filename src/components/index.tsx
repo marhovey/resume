9b0ab4eda16385000b0069dbe8c1c90e5b33b203
marhovey <1063065755@qq.com>
2020-03-31 22:16:33 +0800

    init

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Resume extends React.Component{
+  constructor(props: any){
+    super(props)
+  }
+
+  render(){
+    return (
+      <div className="resume">
+
+      </div>
+    )
+  }
+}
